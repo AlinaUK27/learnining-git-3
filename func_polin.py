9ad64c1a7e1939a1c141b93dc6d606aa31b1d9b8
Alina Somak <mishchya@gmail.com>
2023-03-09 19:47:46 +0100

    Add docstring

--- func_polin.py
+++ func_polin.py
@@ -1,3 +1,8 @@
 def polin (str):
-    return str == str[::-1]
+     '''
+        Print True if argument is palindrome, or False if it isn't palindrome.
+        Argument: "радар"
+        Invert str and compare.
+    '''    
+     return str == str[::-1]
 print(polin("радар"))
